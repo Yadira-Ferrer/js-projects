db311fd12ce8c6a489395e2e46a19885f61c7ba9
Yadira Ferrer <yyfm.95@gmail.com>
2023-02-07 15:17:42 -0600

    Guess my number project complete

--- 04-Guess-My-Number/script.js
+++ 04-Guess-My-Number/script.js
@@ -1,16 +1,71 @@
 'use strict';
 
+const getRandomNumber = max => Math.trunc(Math.random() * max) + 1;
+
 const minValue = 1;
 const maxValue = 20;
 
-document.querySelector('.guess').setAttribute('min', minValue);
-document.querySelector('.guess').setAttribute('max', maxValue);
+let secretNumber = getRandomNumber(maxValue);
+let score = 20;
+let highScore = 0;
 
+//* Set min and max value of input
+let scoreSpan = document.querySelector('.score');
+let inputGuess = document.querySelector('.guess');
+inputGuess.setAttribute('min', minValue);
+inputGuess.setAttribute('max', maxValue);
+
+const displayMessage = message => {
+  document.querySelector('.message').textContent = message;
+};
+
+const changeNumber = (number, width) => {
+  document.querySelector('.number').style.width = width;
+  document.querySelector('.number').textContent = number;
+};
+
+const changeBodyColor = color => {
+  document.querySelector('body').style.backgroundColor = color;
+};
+
+//* Check button 'click' event
 document.querySelector('.check').addEventListener('click', function () {
   const guessValue = Number(document.querySelector('.guess').value);
 
+  //? When there is no input
   if (!guessValue) {
-    document.querySelector('.message').textContent = '⛔ No Number!';
-  } else {
+    displayMessage('⛔ No Number!');
+    //? When player wins
+  } else if (guessValue === secretNumber) {
+    displayMessage('🎉 Correct Number!!!');
+    changeNumber(secretNumber, '30rem');
+    changeBodyColor('#60b347');
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+    //? When guess value is different
+  } else if (guessValue !== secretNumber) {
+    if (score > 1) {
+      displayMessage(
+        guessValue > secretNumber ? '⬆️ Too high...' : '⬇️ Too low...'
+      );
+      score--;
+      scoreSpan.textContent = score;
+    } else {
+      displayMessage('☹️ You lost the game!');
+      scoreSpan.textContent = 0;
+    }
   }
+});
+
+//* Again button 'click' event
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = getRandomNumber(maxValue);
+  inputGuess.value = '';
+  scoreSpan.textContent = score;
+  changeNumber('?', '15rem');
+  displayMessage('🤔 Start guessing...');
+  changeBodyColor('#222');
 });
