89d6a9538a21f4ce145cd62e037d08f6b08526ca
Yadira Ferrer <yyfm.95@gmail.com>
2023-02-08 11:39:15 -0600

    Modal project complete

--- 05-Modal/script.js
+++ 05-Modal/script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let btn of btnsOpenModal) {
+  btn.addEventListener('click', openModal);
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function ({ key }) {
+  const isModalVisible = !modal.classList.contains('hidden');
+  if (key === 'Escape' && isModalVisible) closeModal();
+});
